845e240281d5b9c800dbc1c4ab1c49685147c633
Andy Cole <andy@workportfolio.co.uk>
2011-08-14 00:58:34 +0100

    test git hook

--- admin/github_post_receive.php
+++ admin/github_post_receive.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /kunden/homepages/30/d170410374/htdocs/JBI/ && git reset --hard HEAD && git pull' );
 }
 
 ?>hey dawg

2c6cbce506b5ca631f8bfe8f8e92b7d9a6216e61
Andy Cole <andy@workportfolio.co.uk>
2011-08-14 11:51:49 +0100

    back-tick version of git hook

--- admin/github_post_receive.php
+++ admin/github_post_receive.php
@@ -1,18 +1,6 @@
 <?php
-
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /kunden/homepages/30/d170410374/htdocs/JBI/ && git reset --hard HEAD && git pull' );
-}
-
-if ( $_POST['test'] ) {
-  shell_exec( 'git reset --hard HEAD && git pull' );
-}
-
-
+	// Use in the "Post-Receive URLs" section of your GitHub repo.
+	if ( $_POST['payload'] ) {
+	  `git reset --hard HEAD && git pull`;
+	}
 ?>
-<form action="/admin/github_post_receive.php">
-	<input type="text" name="test" id="test"  />
-	<input type="submit" />	
-</form>

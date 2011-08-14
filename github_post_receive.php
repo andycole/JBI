d875fddc185fa20a1309d431e10829af0a66ad4c
Andy Cole <andy@workportfolio.co.uk>
2011-08-14 13:55:24 +0100

    error check git hook

--- github_post_receive.php
+++ github_post_receive.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('error_reporting', E_ALL);
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if($_POST['payload']) {
+if((isset($_POST['payload'])) {
 	shell_exec('cd /kunden/homepages/30/d170410374/htdocs/JBI && git reset --hard HEAD && git pull');
 	
 	$to = "andy@workportfolio.co.uk";
@@ -9,4 +9,7 @@ if($_POST['payload']) {
 	$body = "Github pushed changes to JBI automatically.";
 	mail($to, $subject, $body);
 	
-}?>
+} else {
+	echo 'failed';
+}
+?>

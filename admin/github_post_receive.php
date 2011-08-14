6be537f3ecf9e6e10c15eee77551e52d7fa55f85
Andy Cole <andy@workportfolio.co.uk>
2011-08-14 12:02:50 +0100

    added mail function to git hook

--- admin/github_post_receive.php
+++ admin/github_post_receive.php
@@ -1,6 +1,11 @@
 <?php
-	// Use in the "Post-Receive URLs" section of your GitHub repo.
-	if ( $_POST['payload'] ) {
-	  `git reset --hard HEAD && git pull`;
-	}
-?>
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+if($_POST['payload']) {
+	`git reset --hard HEAD && git pull`;
+	
+	$to = "andy@workportfolio.co.uk";
+	$subject = "[JBI Updated] - " + date("r");
+	$body = "Github pushed changes to JBI automatically.";
+	mail($to, $subject, $body);
+	
+}?>
